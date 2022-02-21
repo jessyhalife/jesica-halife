f9e8100ca0f8df9304e0b491f3d3567487263d7a
Jesica Halife <jesica.halife@aerolab.co>
2022-02-21 19:57:37 -0300

    wip: refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import "../styles/globals.css";
-import "../styles/Home.scss";
+//import "../styles/globals.css";
+//import "../styles/Home.scss";
+import '../styles/styles.css';
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import { BsMoon, BsSun } from "react-icons/bs";
 import Wordle from "../components/Wordle";
@@ -21,56 +21,54 @@ export default function Home() {
     if (darkTheme) setTheme("dark");
   }, []);
 
-  function handleLanguageSwitch () {
-    Mixpanel.track("Switched language", {lang: lang === "en" ? "es" : "en"}); 
+  function handleLanguageSwitch() {
+    Mixpanel.track("Switched language", { lang: lang === "en" ? "es" : "en" });
     setLang(prev => prev === "en" ? "es" : "en")
   }
   return (
     <>
-      <div id="container" className="container">
-        <Head>
-          <title>Jesica Halife</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className="main">
-          <div className="description">
-            <div className="options">
-              <button onClick={handleLanguageSwitch}   className="language">{lang === "en" ? "ESP" : "ENG"}</button>
-              <button
-                className="toggleTheme"
-                onClick={() =>
-                  setTheme((theme) => (theme === "light" ? "dark" : "light"))
-                }
-              >
-                {theme === "light" ? (
-                  <BsMoon size={24} color="#121212" />
-                ) : (
-                  <BsSun size={24} color="whitesmoke" />
-                )}
-              </button>
-            </div>
-            <h1>{strings.hi[lang]} 👋🏻 {lang === "en" ? `, ${" "}` : <br/>}{strings.Iam[lang]} Jesica Halife</h1>
-            <div className="subtitle">
-              <h2>{strings.ibuild[lang]}</h2>
-              {lang === "en" && <Wordle words={strings.wordleOpts["en"]} word={strings.wordle["en"]}/>}
-              {lang === "es" && <Wordle words={strings.wordleOpts["es"]} word={strings.wordle["es"]}/>}
-            </div>
+      <Head>
+        <title>Jesica Halife</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div id="container">
+        <header>
+          <button onClick={handleLanguageSwitch} className="language">{lang === "en" ? "ESP" : "ENG"}</button>
+          <button
+            className="toggleTheme"
+            onClick={() =>
+              setTheme((theme) => (theme === "light" ? "dark" : "light"))
+            }
+          >
+            {theme === "light" ? (
+              <BsMoon size={24} color="#121212" />
+            ) : (
+              <BsSun size={24} color="whitesmoke" />
+            )}
+          </button>
+        </header>
+        <main>
+          <section>
+            <h1>{strings.hi[lang]} 👋🏻 {lang === "en" ? `, ${" "}` : <br />}{strings.Iam[lang]} Jesica Halife</h1>
+            <h2>{strings.ibuild[lang]}</h2>
+            {lang === "en" && <Wordle words={strings.wordleOpts["en"]} word={strings.wordle["en"]} />}
+            {lang === "es" && <Wordle words={strings.wordleOpts["es"]} word={strings.wordle["es"]} />}
             <code className="code">
               fullstack software engineer
             </code>
-          </div>
-          <section>
-            <h4>{strings.currently[lang]} &nbsp; 🔭</h4>
-            <Technologies />
           </section>
-          <div className="mail">
-            <a onClick={()=> Mixpanel.track("Linkedin click")} target="_blank" href="https://www.linkedin.com/in/jesica-halife/">Linkedin</a>
-            <a onClick={()=> Mixpanel.track("Github click")} href="https://github.com/jessyhalife" target="_blank">Github</a>
-            <a onClick={()=> Mixpanel.track("Email click")} href="mailto:halife.jessy@gmail.com">{strings.email[lang]}</a>
-          </div>
-        </main>
+          <h4>{strings.currently[lang]} &nbsp; 🔭</h4>
+          <Technologies />
+          <a onClick={() => Mixpanel.track("Linkedin click")} target="_blank" href="https://www.linkedin.com/in/jesica-halife/">Linkedin</a>
+          <a onClick={() => Mixpanel.track("Github click")} href="https://github.com/jessyhalife" target="_blank">Github</a>
+          <a onClick={() => Mixpanel.track("Email click")} href="mailto:halife.jessy@gmail.com">{strings.email[lang]}</a>
 
-        <style jsx>{`
+        </main>
+        <footer>
+          <span>Made with ❤️ &nbsp; by @jessyhalife</span>
+          <span>{new Date().getFullYear()}</span>
+        </footer>
+        {/* <style jsx>{`
       .subtitle {
         display: flex;
         flex-direction: row;
@@ -90,11 +88,7 @@ export default function Home() {
         font-size: 0.875rem;
       }
       
-      `}</style>
-        <footer>
-          <span>Made with ❤️ &nbsp; by @jessyhalife</span>
-          <span>{new Date().getFullYear()}</span>
-        </footer>
+      `}</style> */}
       </div>
 
     </>
