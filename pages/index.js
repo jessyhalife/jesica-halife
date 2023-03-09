206e8073d6d66fd5acfc3df4d80569fd313e3caf
Jesica Halife <jesica.halife@aerolab.co>
2023-03-09 09:11:50 -0300

    fix: spacing

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,27 @@
-import { useEffect, useState } from "react";
-import Head from "next/head";
-import { BsMoon, BsSun } from "react-icons/bs";
-import Wordle from "../components/Wordle";
-import Technologies from "../components/Technologies";
-import strings from '../strings.json';
-import { Mixpanel } from "../mixpanel";
+import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { BsMoon, BsSun } from 'react-icons/bs'
+import Wordle from '../components/Wordle'
+import Technologies from '../components/Technologies'
+import strings from '../strings.json'
+import { Mixpanel } from '../mixpanel'
 
-export default function Home() {
-  const [theme, setTheme] = useState("light");
-  const [lang, setLang] = useState("en")
+export default function Home () {
+  const [theme, setTheme] = useState('light')
+  const [lang, setLang] = useState('en')
   useEffect(() => {
-    if (theme === "light")
-      document.getElementById("container").classList.remove("dark");
-    else document.getElementById("container").classList.add("dark");
-  }, [theme]);
+    if (theme === 'light') { document.getElementById('container').classList.remove('dark') } else document.getElementById('container').classList.add('dark')
+  }, [theme])
 
   useEffect(() => {
-    Mixpanel.track('Page View');
-    const darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
-    if (darkTheme) setTheme("dark");
-  }, []);
+    Mixpanel.track('Page View')
+    const darkTheme = window.matchMedia('(prefers-color-scheme: dark)')
+    if (darkTheme) setTheme('dark')
+  }, [])
 
-  function handleLanguageSwitch() {
-    Mixpanel.track("Switched language", { lang: lang === "en" ? "es" : "en" });
-    setLang(prev => prev === "en" ? "es" : "en")
+  function handleLanguageSwitch () {
+    Mixpanel.track('Switched language', { lang: lang === 'en' ? 'es' : 'en' })
+    setLang(prev => prev === 'en' ? 'es' : 'en')
   }
   return (
     <>
@@ -32,7 +30,7 @@ export default function Home() {
           <title>Jesica Halife</title>
           <link rel="icon" href="/favicon.ico" />
           {/* <!-- Primary Meta Tags --> */}
-          <title>Hi 👋🏻 , I'm Jesica Halife</title>
+          <title>Hi 👋🏻 , I&apos;m Jesica Halife</title>
           <meta name="title" content="Hi 👋🏻 , I'm Jesica Halife" />
           <meta name="description" content="Fullstack software Engineer" />
 
@@ -53,25 +51,27 @@ export default function Home() {
         <main className="main">
           <div className="description">
             <div className="options">
-              <button onClick={handleLanguageSwitch} className="language">{lang === "en" ? "ESP" : "ENG"}</button>
+              <button onClick={handleLanguageSwitch} className="language">{lang === 'en' ? 'ESP' : 'ENG'}</button>
               <button
                 className="toggleTheme"
                 onClick={() =>
-                  setTheme((theme) => (theme === "light" ? "dark" : "light"))
+                  setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))
                 }
               >
-                {theme === "light" ? (
+                {theme === 'light'
+                  ? (
                   <BsMoon size={24} color="#121212" />
-                ) : (
+                    )
+                  : (
                   <BsSun size={24} color="whitesmoke" />
-                )}
+                    )}
               </button>
             </div>
-            <h1>{strings.hi[lang]} 👋🏻 {lang === "en" ? `, ${" "}` : <br />}{strings.Iam[lang]} Jesica Halife</h1>
+            <h1>{strings.hi[lang]} 👋🏻 {lang === 'en' ? `, ${' '}` : <br />}{strings.Iam[lang]} Jesica Halife</h1>
             <div className="subtitle">
               <h2>{strings.ibuild[lang]}</h2>
-              {lang === "en" && <Wordle words={strings.wordleOpts["en"]} word={strings.wordle["en"]} />}
-              {lang === "es" && <Wordle words={strings.wordleOpts["es"]} word={strings.wordle["es"]} />}
+              {lang === 'en' && <Wordle words={strings.wordleOpts.en} word={strings.wordle.en} />}
+              {lang === 'es' && <Wordle words={strings.wordleOpts.es} word={strings.wordle.es} />}
             </div>
             <code className="code">
               fullstack software engineer
@@ -82,9 +82,9 @@ export default function Home() {
             <Technologies />
           </section>
           <div className="mail">
-            <a onClick={() => Mixpanel.track("Linkedin click")} target="_blank" href="https://www.linkedin.com/in/jesica-halife/">Linkedin</a>
-            <a onClick={() => Mixpanel.track("Github click")} href="https://github.com/jessyhalife" target="_blank">Github</a>
-            <a onClick={() => Mixpanel.track("Email click")} href="mailto:halife.jessy@gmail.com">{strings.email[lang]}</a>
+            <a onClick={() => Mixpanel.track('Linkedin click')} target="_blank" href="https://www.linkedin.com/in/jesica-halife/" rel="noreferrer">Linkedin</a>
+            <a onClick={() => Mixpanel.track('Github click')} href="https://github.com/jessyhalife" target="_blank" rel="noreferrer">Github</a>
+            <a onClick={() => Mixpanel.track('Email click')} href="mailto:halife.jessy@gmail.com">{strings.email[lang]}</a>
           </div>
         </main>
 
@@ -110,11 +110,11 @@ export default function Home() {
       
       `}</style>
         <footer>
-          <span>Made with ❤️ &nbsp; by @jessyhalife</span>
+          <span>Made with ❤️&nbsp;by @jessyhalife</span>
           <span>{new Date().getFullYear()}</span>
         </footer>
       </div>
 
     </>
-  );
+  )
 }
